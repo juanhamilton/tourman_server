d11f8b8d13075b716e878927ea2685bba8d483af
Juan Verdugo <jverdugo2002@gmail.com>
2024-05-01 13:11:08 -0400

    Se crea Modelo

---
+++ models/tournament.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate');
+
+const TournamentSchema = mongoose.Schema({
+    Name: String,
+    Star_Date: Date,
+    End_Date: Date,
+    Format: String,
+    Reward: String,
+    Prize: Number,
+    Code: String
+})
+
+TournamentSchema.plugin(mongoosePaginate);
+
+module.exports = mongoose.model("Tournament", TournamentSchema);

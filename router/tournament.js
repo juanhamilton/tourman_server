3affbb741792229921839639d2ad114832495433
Juan Verdugo <jverdugo2002@gmail.com>
2024-05-01 13:20:47 -0400

    Se crea Tournament Router

---
+++ router/tournament.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const TournamentController = require("../controllers/tournament");
+const md_auth = require("../middlewares/authenticated");
+
+const api = express.Router();
+
+
+api.post("/tournament", [md_auth.asureAuth], TournamentController.createMenu);
+// api.get("/tournament", [md_auth.asureAuth], MenuController.getMenu)
+// api.patch("/tournament/:id",  [md_auth.asureAuth], TournamentController.updateMenu)
+// api.delete("/tournament/:id",  [md_auth.asureAuth], TournamentController.deleteMenu)
+
+
+module.exports = api;

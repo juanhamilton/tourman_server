7bc7d9d5436d19be0338e0a3e821461badf34d94
Juan Verdugo <jverdugo2002@gmail.com>
2024-05-06 23:50:26 -0400

    Se quita log en carga imagen

--- utils/image.js
+++ utils/image.js
@@ -1,14 +1,14 @@
-function getFilePath(file){
+function getFilePath(file) {
     let separador
-    const filePath = file.path;    
-    if(filePath.indexOf("\\")>0) {
+    const filePath = file.path;
+    if (filePath.indexOf("\\") > 0) {
         separador = "\\";
     } else {
         separador = "/";
     }
-console.log(file);
+
     const fileSplit = filePath.split(separador);
-console.log(fileSplit);
+
     return `${fileSplit[1]}/${fileSplit[2]}`;
 
 }
